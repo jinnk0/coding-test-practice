49638dd82aa5acc4dbbe502ab5fbec165c6d8776
jinnk0 <jin8073@gmail.com>
2024-10-04 22:05:16 +0900

    solve: [실버2] 스택/큐 - 백준 28066

---
+++ 스택&큐/28066.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+N, K = map(int, input().split())
+queue = deque([i for i in range(N)])
+
+while (len(queue) > 1):
+    if len(queue) > K:
+        queue.append(queue.popleft())
+        for _ in range(K-1):
+            queue.popleft()
+    else:
+        queue = deque([queue.popleft()])
+
+print(queue.popleft()+1)
