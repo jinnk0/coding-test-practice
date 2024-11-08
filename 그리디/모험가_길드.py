b28f9b07e15bb02845e03e1cee6a68e8de77360e
jinnk0 <jin8073@gmail.com>
2024-11-08 16:32:21 +0900

    solve: 그리디 - 모험가 길드

---
+++ 그리디/모험가_길드.py
@@ -0,0 +1,20 @@
+'''
+# 출처 : 이것이 취업을 위한 코딩테스트다
+# 문제 유형 : 그리디
+# 입력
+    - N : 모험가의 수 (1 <= N <= 100,000)
+    - 각 모험가의 공포도 값 N 이하의 N개의 자연수
+# 출력
+    - 여행을 떠날 수 있는 그룹 수의 최댓값
+'''
+N = int(input())
+members = sorted(list(map(int, input().split())))
+count = 0
+while members:
+    member = members.pop()
+    if member <= len(members):
+        for _ in range(member-1):
+            members.pop()
+            count += 1
+
+print(count)
