d6323b5a82753eb01c48facd6a467d5740fb5488
jinnk0 <jin8073@gmail.com>
2025-03-19 19:58:56 +0900

    solve: 백준 10891 차이를 최대로 (실버2)

---
+++ 완전탐색/10819_차이를 최대로.py
@@ -0,0 +1,53 @@
+'''
+# 출처 : 백준 10891 차이를 최대로 (실버2)
+# 유형 : 브루트포스, 백트래킹(완전탐색)
+'''
+# 가능한 모든 순열 구하기
+# 차이의 최대값 구하기
+
+'''from itertools import permutations
+
+N = int(input())
+numbers = list(map(int, input().split()))
+
+cases = list(permutations(numbers, N)) # 가능한 모든 순열 구하기
+
+max_diff = - 999
+for case in cases:
+    diff = 0
+    for i in range(len(case)-1):
+        diff += abs(case[i] - case[i+1])
+    max_diff = max(max_diff, diff) # 차이의 최대값 저장 
+
+print(max_diff)'''
+
+'''
+# 다른 방법
+
+# 가능한 모든 순열의 차이의 최대값 탐색 (백트래킹 bfs 이용)
+'''
+N = int(input())
+numbers = list(map(int, input().split()))
+
+def bfs(depth, prev, visited, diff_sum, max_diff):
+    if depth == N:
+        return max(diff_sum, max_diff)
+    
+    diff = -float('inf')
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = True
+            diff = max(diff, bfs(depth+1, numbers[i], visited, diff_sum + abs(prev - numbers[i]), max_diff))
+            visited[i] = False
+        
+    return diff
+            
+max_diff = -999
+for i in range(N):
+    visited = [False] * N
+    visited[i] = True
+    max_diff = bfs(1, numbers[i], visited, 0, max_diff)
+
+print(max_diff)
+
+
