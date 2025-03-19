a44fe74e4fdb4a94e0a3bcd7c44255f186273a44
jinnk0 <jin8073@gmail.com>
2025-03-19 22:45:08 +0900

    chore: 백준 10819 차이를 최대로 (실버2) 오타 수정

--- 완전탐색/10819_차이를 최대로.py
+++ 완전탐색/10819_차이를 최대로.py
@@ -5,7 +5,7 @@
 # 가능한 모든 순열 구하기
 # 차이의 최대값 구하기
 
-'''from itertools import permutations
+from itertools import permutations
 
 N = int(input())
 numbers = list(map(int, input().split()))
@@ -19,17 +19,17 @@ for case in cases:
         diff += abs(case[i] - case[i+1])
     max_diff = max(max_diff, diff) # 차이의 최대값 저장 
 
-print(max_diff)'''
+print(max_diff)
 
 '''
 # 다른 방법
 
-# 가능한 모든 순열의 차이의 최대값 탐색 (백트래킹 bfs 이용)
+# 가능한 모든 순열의 차이의 최대값 탐색 (백트래킹 dfs 이용)
 '''
 N = int(input())
 numbers = list(map(int, input().split()))
 
-def bfs(depth, prev, visited, diff_sum, max_diff):
+def dfs(depth, prev, visited, diff_sum, max_diff):
     if depth == N:
         return max(diff_sum, max_diff)
     
@@ -37,7 +37,7 @@ def bfs(depth, prev, visited, diff_sum, max_diff):
     for i in range(N):
         if not visited[i]:
             visited[i] = True
-            diff = max(diff, bfs(depth+1, numbers[i], visited, diff_sum + abs(prev - numbers[i]), max_diff))
+            diff = max(diff, dfs(depth+1, numbers[i], visited, diff_sum + abs(prev - numbers[i]), max_diff))
             visited[i] = False
         
     return diff
@@ -46,7 +46,7 @@ max_diff = -999
 for i in range(N):
     visited = [False] * N
     visited[i] = True
-    max_diff = bfs(1, numbers[i], visited, 0, max_diff)
+    max_diff = dfs(1, numbers[i], visited, 0, max_diff)
 
 print(max_diff)
 
