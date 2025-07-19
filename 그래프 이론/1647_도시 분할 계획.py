e3c0a6e096b0683221f0bd3f37376f0c029a47a5
jinnk0 <jin8073@gmail.com>
2025-07-19 17:33:03 +0900

    solve: [골드4] 백준 1647 도시 분할 계획 문제 풀이

--- 그래프 이론/1647_도시 분할 계획.py
+++ 그래프 이론/1647_도시 분할 계획.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
@@ -10,17 +13,14 @@ def union_parent(parent, a, b):
         parent[b] = a
     else:
         parent[a] = b
-    
-v, e = map(int, input().split())
-parent = [0] * (v+1)
+
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
 
 edges = []
 result = 0
 
-for i in range(1, v+1):
-    parent[i] = i
-
-for _ in range(e):
+for _ in range(m):
     a, b, cost = map(int, input().split())
     edges.append((cost, a, b))
 
