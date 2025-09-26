ea282cfad9cfe1c39c2d8d7224e2c5fa2e68acc7
jinnk0 <jin8073@gmail.com>
2025-09-26 16:59:46 +0900

    solve: 프로그래머스 lv.3 등굣길 문제 풀이

--- 다이나믹 프로그래밍/등굣길.py
+++ 다이나믹 프로그래밍/등굣길.py
@@ -1,12 +1,20 @@
+'''
+# 프로그래머스 lv.3 등굣길
+# m x n 격자 모양의 길에서 (1,1)은 집, (m, n)은 학교
+# 물에 일부 지역이 잠겼을 때 집에서 학교까지의 최단 경로 개수
+# 오른쪽 또는 아래쪽으로만 이동 가능
+# 집과 학교는 물에 잠기지 않으며, m과 n이 동시에 1인 경우는 주어지지 않음
+# 입력 : m, n (1 <= m, n <= 100), puddles(물에 잠긴 지역 좌표가 담긴 2차원 배열, 길이는 0 이상 10 이하)
+# 출력 : 집에서 학교까지의 최단 경로 개수 % 1,000,000,007
+'''
 def solution(m, n, puddles):
-    DP = [[0] * (m+1) for _ in range(n+1)]
-    DP[1][1] = 1
-    for i in range(1, m+1):
-        for j in range(1, n+1):
+    matrix = [[0] * (m + 1) for _ in range(n + 1)]
+    matrix[1][1] = 1
+    puddles.append([1, 1])
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
             if [i, j] in puddles:
                 continue
-            if i-1 > 0 and i-1 <= m:
-                DP[j][i] += DP[j][i-1]
-            if j-1 > 0 and j-1 <= n:
-                DP[j][i] += DP[j-1][i]
-    return DP[n][m] % 1000000007
+            matrix[i][j] = matrix[i-1][j] + matrix[i][j-1]
+
+    return matrix[n][m] % 1_000_000_007
